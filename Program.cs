65d2c8ce1c021044c01065e74133405307d7839c
Maksat Kuanyshbekov <kuanyshbekovm@gmail.com>
2017-09-09 14:47:19 +0600

    Third

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace gitntplabs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!");
         }
     }
 }
